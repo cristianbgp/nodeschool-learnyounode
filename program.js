75acd8e188d4d1af8cb5102e3652b07e47f8bb8a
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-04-13 17:25:18 -0500

    Add exercise 2

--- program.js
+++ program.js
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD");
+sum = 0;
+for (let i = 0; i < process.argv.length; i++) {
+  const element = process.argv[i];
+  if (i === 0 || i === 1) {
+  } else {
+    sum += Number(element);
+  }
+}
+console.log(sum);
